724efbba2e26feb56619250e6876ef58aa53e477
wangyonghua <1770220534@qq.com>
2017-10-06 12:03:13 +0800

    java api

--- hdfs/src/cn/itcast/hadoop/hdfs/HdfsDemo.java
+++ hdfs/src/cn/itcast/hadoop/hdfs/HdfsDemo.java
@@ -1,4 +1,7 @@
 package cn.itcast.hadoop.hdfs;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,13 +13,30 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
 
 public class HdfsDemo {
-    public static void main(String[] args) throws IOException, URISyntaxException {
-    	FileSystem fs=FileSystem.get(new URI("hdfs://itcast01:9000"), new Configuration());
-    	InputStream stream = fs.open(new Path("/java"));
-    	OutputStream outputStream=new FileOutputStream("F://test");
-    	IOUtils.copyBytes(stream, outputStream, 4096, true);
-    	System.out.println("ok");
+	private FileSystem fs = null;
+
+	@Before
+	public void init() throws IOException, URISyntaxException, InterruptedException {
+		fs = FileSystem.get(new URI("hdfs://itcast01:9000"), new Configuration(), "root");
+	}
+
+	@Test
+	public void testUpload() throws IllegalArgumentException, IOException {
+		InputStream inputStream = new FileInputStream("f://test.txt");
+		OutputStream outputStream = fs.create(new Path("/test"));
+		IOUtils.copyBytes(inputStream, outputStream, 4096, true);
+		System.out.println("ok");
+	}
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		FileSystem fs = FileSystem.get(new URI("hdfs://itcast01:9000"), new Configuration());
+		InputStream stream = fs.open(new Path("/java"));
+		OutputStream outputStream = new FileOutputStream("F://test");
+		IOUtils.copyBytes(stream, outputStream, 4096, true);
+		System.out.println("ok");
 	}
 }
